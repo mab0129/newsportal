1edada90f813002d9cf80585a308bd71382eefc8
mab0129 <76690579+mab0129@users.noreply.github.com>
2026-01-13 19:46:38 +0600

    Update Header.tsx

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
 
   const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   const d = new Date();
-  let day = weekday[d.getDay()];
-  let date = d.getDate();
-  let month = d.getMonth();
-  let year = d.getFullYear();
-  let fulldate =  date +'/' + month + '/' + year;
+  const day = weekday[d.getDay()];
+  const date = d.getDate();
+  const month = d.getMonth();
+  const year = d.getFullYear();
+  const fulldate =  date +'/' + month + '/' + year;
 
   return (
     <div className='grid md:grid-cols-3 sm:grid-cols-1 md:justify-between sm:justify-center content-center mb-2'>
